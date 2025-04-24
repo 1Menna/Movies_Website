29e4d822f62dab83417d66224b6fd555db9d1206
1Menna <meno903010@gmail.com>
2025-04-24 13:37:42 -0700

    -

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ createRoot(document.getElementById('root')).render(
        <ThemeProvider>
           <App />
        </ThemeProvider>
-     </StrictMode>,
+     </StrictMode>
   </BrowserRouter>
   
 )
