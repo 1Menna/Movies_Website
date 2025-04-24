ec6c42dfd1d7baad506fc01c6b5dfdbf82413455
1Menna <meno903010@gmail.com>
2025-04-24 13:36:59 -0700

    add_some_routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
+import { Routes, Route } from "react-router-dom"
+import About from './Pages/About'
+import Contact from './Pages/Contact'
+import SignIn from './Pages/SignIn'
+import SignUp from './Pages/SignUp'
+import Home from './Pages/Home'
+
 function App() {
   return (
     <>
       <div>
           <h1>Movies_Website</h1>
+          <Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/SignUp" element={<SignUp/>}/>
+            <Route path="/SignIn" element={<SignIn/>}/>
+            <Route path="/About" element={<About/>}/>
+            <Route path="/Contact" element={<Contact/>}/>
+          </Routes>
       </div>
     </>
   )
