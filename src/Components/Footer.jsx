7cddd3693631e344b634f7c1687a6275283a292e
1Menna <meno903010@gmail.com>
2025-04-26 20:33:57 +0300

    -

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -5,14 +5,14 @@ const Footer = () => {
   <footer className="footer sm:footer-horizontal bg-neutral text-neutral-content p-10">
   <nav>
     <h6 className="footer-title">Company</h6>
-    <a className="link link-hover">About us</a>
-    <a className="link link-hover">Contact</a>
+    <a className="link link-hover hover:text-info">About us</a>
+    <a className="link link-hover hover:text-info">Contact</a>
   </nav>
   <nav>
     <h6 className="footer-title">Legal</h6>
-    <a className="link link-hover">Terms of use</a>
-    <a className="link link-hover">Privacy policy</a>
-    <a className="link link-hover">Cookie policy</a>
+    <a className="link link-hover hover:text-info">Terms of use</a>
+    <a className="link link-hover hover:text-info">Privacy policy</a>
+    <a className="link link-hover hover:text-info">Cookie policy</a>
   </nav>
 </footer>
   )
