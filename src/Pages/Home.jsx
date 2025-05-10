714d81df3184ec36c7dfe519fe3907477396021f
1Menna <meno903010@gmail.com>
2025-05-10 13:41:41 +0300

    -

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -78,7 +78,7 @@ const Home = () => {
       <div className="relative h-screen flex items-center justify-center">
          {/* Background image with gradient overlay */}
           <div className="absolute inset-0">
-              <div className="absolute inset-0 bg-[url('./public/home.jpg')] bg-cover bg-center"></div>
+              <div className="absolute inset-0 bg-[url('/home.jpg')] bg-cover bg-center"></div>
              <div className="absolute inset-0 bg-gradient-to-t from-black via-black/70 to-transparent"></div>
           </div>
           {/* Content */}
